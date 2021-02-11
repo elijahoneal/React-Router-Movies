c99233f0fbc7921cdb8afd9d0e5b5296da406122
Elijah <elijahoneal@yahoo.com>
2021-02-10 21:35:14 -0500

    Install packages + MovieList router

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import { Route } from 'react-router-dom'
 import SavedList from './Movies/SavedList';
-
+import MovieList from './Movies/MovieList'
 export default function App () {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
   const [movieList, setMovieList] = useState([]);
@@ -14,6 +14,8 @@ export default function App () {
         .then(response => {
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
+          console.log(response.data)
+          setMovieList(response.data)
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -30,7 +32,10 @@ export default function App () {
     <div>
       <SavedList list={[ /* This is stretch */]} />
 
-      <div>Replace this Div with your Routes</div>
+      {/* <div>Replace this Div with your Routes</div> */}
+      <Route path = '/'>
+        <MovieList movies = {movieList} />
+      </Route>
     </div>
   );
 }
